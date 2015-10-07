ba621aee143030a6eea3581d8ad3e8082cc4b67a
leviathon <leviathon@Levs-Mac.local>
2015-10-07 16:30:00 -0700

    keep on dividing the num by 2

---
+++ app.js
@@ -0,0 +1,21 @@
+function powerOfTwo(num) {
+	if(num < 2) {
+		ans = false;
+	}else {
+		while(num >= 2) {
+			var test = num % 2;
+			if(test == 0) {
+				num = num / 2;
+				ans = true;
+			}else {
+				num = 0;
+				ans = false;
+			}
+		}
+	}
+
+	return ans;
+
+}
+
+console.log(powerOfTwo(100));
